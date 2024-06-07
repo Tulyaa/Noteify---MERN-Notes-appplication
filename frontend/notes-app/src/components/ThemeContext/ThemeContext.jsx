e8b72f10e0423851511ceaa0c0a344f281958de3
tulyaa <tulyaa>
2024-06-07 20:36:42 +0530

    frontend and backend commit

---
+++ frontend/notes-app/src/components/ThemeContext/ThemeContext.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+export const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        const savedTheme = localStorage.getItem('theme') || 'light';
+        setTheme(savedTheme);
+        document.body.classList.add(savedTheme);
+    }, []);
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+        document.body.classList.remove(theme);
+        document.body.classList.add(newTheme);
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
